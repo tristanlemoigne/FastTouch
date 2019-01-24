eca17987ef91e6ddbe746612856bf8a1fb8b018f
tristanlemoigne <tristanlemoigne@gmail.com>
2019-01-24 13:47:19 +0100

    Trying to add score

--- app/src/main/java/com/example/fasttouch/MainActivity.java
+++ app/src/main/java/com/example/fasttouch/MainActivity.java
@@ -5,33 +5,40 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private ImageView imageView;
+    private ImageView button;
+    private TextView score;
+    private Integer scoreCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // THIS IS A TEST
 
-        imageView = findViewById(R.id.button);
-
+        button = findViewById(R.id.button);
+        score = findViewById(R.id.score_count);
     }
 
     public void toggleLight(View view) {
-         if(imageView.getTag().toString().equals("button_off")){
-            imageView.setImageResource(R.drawable.button_on);
-            imageView.setTag("button_on");
+         if(button.getTag().toString().equals("button_off")){
+             button.setImageResource(R.drawable.button_on);
+             button.setTag("button_on");
+
+             scoreCount = scoreCount + 1;
+
         } else {
-             imageView.setImageResource(R.drawable.button_off);
-             imageView.setTag("button_off");
+             button.setImageResource(R.drawable.button_off);
+             button.setTag("button_off");
+
+             scoreCount = scoreCount - 1;
         }
 
-        Log.d(LOG_TAG, imageView.getDrawable().toString());
-        Log.d(LOG_TAG, imageView.getResources().toString());
-        Log.d(LOG_TAG, imageView.getTag().toString());
 
+        Log.d(LOG_TAG, scoreCount.toString());
+        score.setText(scoreCount);
     }
 }
