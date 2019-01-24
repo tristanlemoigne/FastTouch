5612d725d7ba86153265965f54a7931a508839f3
tristanlemoigne <tristanlemoigne@gmail.com>
2019-01-24 12:01:56 +0100

    First light working

--- app/src/main/java/com/example/fasttouch/MainActivity.java
+++ app/src/main/java/com/example/fasttouch/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.fasttouch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // THIS IS A TEST
+
+        imageView = findViewById(R.id.button);
+
+    }
+
+    public void toggleLight(View view) {
+         if(imageView.getTag().toString().equals("button_off")){
+            imageView.setImageResource(R.drawable.button_on);
+            imageView.setTag("button_on");
+        } else {
+             imageView.setImageResource(R.drawable.button_off);
+             imageView.setTag("button_off");
+        }
+
+        Log.d(LOG_TAG, imageView.getDrawable().toString());
+        Log.d(LOG_TAG, imageView.getResources().toString());
+        Log.d(LOG_TAG, imageView.getTag().toString());
+
     }
 }
