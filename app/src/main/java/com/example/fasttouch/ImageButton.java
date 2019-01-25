17c5fe33960d329ea4916e7acf293815db229a4f
tristanlemoigne <tristanlemoigne@gmail.com>
2019-01-25 13:54:13 +0100

    Adding Score

--- app/src/main/java/com/example/fasttouch/ImageButton.java
+++ app/src/main/java/com/example/fasttouch/ImageButton.java
@@ -5,9 +5,12 @@ import android.os.CountDownTimer;
 import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
 import android.util.AttributeSet;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 
 import java.util.Random;
 import java.util.Timer;
@@ -17,29 +20,36 @@ public class ImageButton extends AppCompatImageView {
 
     private static final String LOG_TAG = ImageButton.class.getSimpleName();
 
-    public ImageButton(Context context) {
+    public ImageButton(Context context, Integer tileSize) {
         super(context);
-//        this.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                toggleLight();
-//            }
-//        });
 
+        // Set Layout params
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(tileSize, tileSize);
+//        layoutParams.setMargins(20,20,20,20);
+        this.setLayoutParams(layoutParams);
+
+        // Set tag
         this.setTag("button_off");
 
+        // Set Timer
         Random random = new Random();
-        int randomTimer = random.nextInt(1000 - 100) + 100;
-
+        int randomTimer = random.nextInt(2000 - 1000) + 1000;
 
         new Timer().scheduleAtFixedRate(new TimerTask(){
             @Override
             public void run() {
                 toggleLight();
-                randomTimer = random.nextInt(1000 - 100) + 100;
 
             }
 
         },0,randomTimer);
+
+        // Set click listener
+        this.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleClick();
+            }
+        });
 
     }
 
@@ -51,22 +61,24 @@ public class ImageButton extends AppCompatImageView {
         super(context, attrs, defStyleAttr);
     }
 
-
-
     public void toggleLight() {
-
         if(this.getTag().toString().equals("button_off")){
             this.setImageResource(R.drawable.button_on);
             this.setTag("button_on");
-//            MainActivity.scoreCountDown();
         } else {
             this.setImageResource(R.drawable.button_off);
             this.setTag("button_off");
-//            MainActivity.scoreCountUp();
         }
+//         Log.d(LOG_TAG, "_______________" + this.getTag().toString());
+    }
 
-         // Log.d(LOG_TAG, MainActivity.getScoreCount().toString());
-
-         Log.d(LOG_TAG, "_______________" + this.getTag().toString());
+    public void handleClick(){
+        if(this.getTag().toString().equals("button_off")){
+            MainActivity.scoreCountDown();
+        } else {
+            this.setImageResource(R.drawable.button_off);
+            this.setTag("button_off");
+            MainActivity.scoreCountUp();
+        }
     }
 }
--- app/src/main/java/com/example/fasttouch/MainActivity.java
+++ app/src/main/java/com/example/fasttouch/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.fasttouch;
 
+import android.graphics.Color;
+import android.graphics.Point;
 import android.support.constraint.ConstraintSet;
 import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
@@ -15,74 +19,63 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private ImageView button;
-    private TextView score;
+    private GridLayout gridLayout;
+    public static TextView score;
     public static Integer scoreCount = 0;
-    private Integer buttonsCount = 24;
-    private ImageView[] theButtons = new ImageView[buttonsCount];
 
+    private Integer columnsCount = 4;
+    private Integer rowsCount = 6;
+    private Integer buttonsCount = columnsCount * (rowsCount -1);
+    private Integer tileSize = 0;
+
+    //    private Integer buttonsCount = 12;
+    private ImageView[] theButtons = new ImageView[buttonsCount];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set bg color
+        getWindow().getDecorView().setBackgroundColor(Color.parseColor("#393152"));
 
+        // Set score
         score = findViewById(R.id.score_count);
+        score.setText(String.valueOf(scoreCount));
 
-        GridLayout picGL = new GridLayout(this);
-        picGL.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
-        picGL.setColumnCount(4);
-        picGL.setRowCount(3);
+        // Set layout params
+        android.support.v7.widget.GridLayout gridLayout = (android.support.v7.widget.GridLayout)findViewById(R.id.grid_layout);
+        gridLayout.setColumnCount(columnsCount);
+        gridLayout.setRowCount(rowsCount);
 
-//        ConstraintSet set = new ConstraintSet();
-//        set.connect(picGL.getId(), ConstraintSet.TOP,
-//                ConstraintSet.PARENT_ID, ConstraintSet.TOP, 70);
-//        set.applyTo(picGL);
 
-        // picGL.setLayoutParams(new GridLayout.LayoutParams(GridLayout.spec(1),GridLayout.spec(1)));
+        // Get columns / rows dimensions
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+//        tileSize = size.x / columnsCount - ((rowsCount * size.x / columnsCount) - size.y);
+        tileSize = size.x / columnsCount;
 
-//        picLL.layout(0, 0, 100, 100);
-//        picLL.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
-//        picLL.setOrientation(LinearLayout.HORIZONTAL);
+
 
         for (int i = 0; i < buttonsCount; i++) {
-
-            theButtons[i] = new ImageButton(this);
+            theButtons[i] = new ImageButton(this, tileSize);
             theButtons[i].setImageResource(R.drawable.button_off);
-            theButtons[i].layout(i * 100, i * 100, 0, 0);
 
+            Log.d(LOG_TAG, "///////___________////////" +"x:"+ size.x + "y:" + size.y);
 
-            Log.d(LOG_TAG, "///////___________////////");
-            Log.d(LOG_TAG, theButtons[i].toString());
-
-            picGL.addView(theButtons[i]);
-
-
-            Log.d(LOG_TAG, "the buttons: " + theButtons[i].toString());
-            setContentView(picGL);
+            gridLayout.addView(theButtons[i]);
         }
+    }
 
+    static public void scoreCountUp() {
+        scoreCount++;
         score.setText(String.valueOf(scoreCount));
     }
 
-    static public void scoreCountUp()
-
-    {
-        scoreCount++;
-    }
-
-    static public void scoreCountDown()
-
-    {
+    static public void scoreCountDown() {
         scoreCount--;
+        score.setText(String.valueOf(scoreCount));
     }
-
-//    public Integer getScoreCount() {
-//        return scoreCount;
-//    }
-//
-//    public static void setScoreCount(Integer scoreCount) {
-//        MainActivity.scoreCount = scoreCount;
-//    }
 }
 
