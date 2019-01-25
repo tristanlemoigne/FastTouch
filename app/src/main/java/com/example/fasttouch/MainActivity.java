c83edc0f951f49f2a07a3c6cfcccf05d2e303e15
mpicod <mrtn.pcd@gmail.com>
2019-01-25 16:59:13 +0100

    Solène check

--- app/src/main/java/com/example/fasttouch/MainActivity.java
+++ app/src/main/java/com/example/fasttouch/MainActivity.java
@@ -2,20 +2,17 @@ package com.example.fasttouch;
 
 import android.graphics.Color;
 import android.graphics.Point;
-import android.os.CountDownTimer;
-import android.support.constraint.ConstraintSet;
-import android.support.v4.widget.ImageViewCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -35,11 +32,28 @@ public class MainActivity extends AppCompatActivity {
 
     //    private Integer buttonsCount = 12;
     private ImageButton[] theButtons = new ImageButton[buttonsCount];
+    private View resetButton;
+
+    // some transient state for the activity instance
+    private ArrayList <String> mGameState;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initArray();
+//        if (savedInstanceState != null) {
+//            for (int i = 0; i < buttonsCount; i++){
+//                mGameState.set(i, savedInstanceState.getString(theButtons[i].getTag().toString()));
+//
+//            }
+//        }
+
+
+
 
         // Set bg color
         getWindow().getDecorView().setBackgroundColor(Color.parseColor("#393152"));
@@ -47,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         // Find score and timer
         score = findViewById(R.id.score_count);
         timer = findViewById(R.id.timer_count);
+        resetButton = findViewById(R.id.resetButton);
 
         // Set layout params
         android.support.v7.widget.GridLayout gridLayout = (android.support.v7.widget.GridLayout)findViewById(R.id.grid_layout);
@@ -86,10 +101,28 @@ public class MainActivity extends AppCompatActivity {
                     theButtons[i].stopInterval();
                     theButtons[i].setOnClickListener(null);
                 }
+//                resetButton.setVisibility(View.VISIBLE);
             }
         }.start();
     }
 
+    public void initArray(){
+        mGameState = new ArrayList<>();
+        for (int i =0; i>0; i++){
+            mGameState.set(i, "button_off");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        for (int i = 0; i < buttonsCount; i++) {
+            outState.putString(theButtons[i].getTag().toString(), theButtons[i].getTag().toString() );
+            Log.d(LOG_TAG, "on save save instance ");
+        }
+        super.onSaveInstanceState(outState);
+
+
+    }
     static public void scoreCountUp() {
         scoreCount++;
         score.setText(String.valueOf(scoreCount));
@@ -98,6 +131,18 @@ public class MainActivity extends AppCompatActivity {
     static public void scoreCountDown() {
         scoreCount--;
         score.setText(String.valueOf(scoreCount));
+    }
+
+    public void resetGame(View view) {
+        Log.d(LOG_TAG, "RESET GAME");
+        for ( int i = 0; i < buttonsCount; i++) {
+            theButtons[i].startInterval();
+//            theButtons[i].setOnClickListener(new View.OnClickListener() {
+//                public void onClick(View v) {
+////                    handleClick();
+//                }
+//            });
+        }
     }
 }
 
